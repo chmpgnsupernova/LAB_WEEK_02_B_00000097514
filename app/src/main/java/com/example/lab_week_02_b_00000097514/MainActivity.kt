dd02b31126304784c68bb154588b411804320250
chmpgnsupernova <brianmariarvin@gmail.com>
2025-09-05 19:44:41 +0700

    Modified

--- app/src/main/java/com/example/lab_week_02_b_00000097514/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_02_b_00000097514/MainActivity.kt
@@ -6,14 +6,30 @@ import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputEditText
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
     companion object {
-        private const val COLOR_KEY = "COLOR_KEY"
+        const val COLOR_KEY = "COLOR_KEY"
+        const val ERROR_KEY = "ERROR_KEY"
     }
 
     private val submitButton: Button
         get() = findViewById(R.id.submit_button)
+
+    // registerForActivityResult untuk ResultActivity
+    private val startForResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { activityResult ->
+            val data = activityResult.data
+            val error = data?.getBooleanExtra(ERROR_KEY, false)
+            if (error == true) {
+                Toast.makeText(
+                    this,
+                    getString(R.string.color_code_input_invalid),
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,7 +49,7 @@ class MainActivity : AppCompatActivity() {
                 } else {
                     val resultIntent = Intent(this, ResultActivity::class.java)
                     resultIntent.putExtra(COLOR_KEY, colorCode)
-                    startActivity(resultIntent)
+                    startForResult.launch(resultIntent) // ganti startActivity
                 }
             } else {
                 Toast.makeText(
--- app/src/main/java/com/example/lab_week_02_b_00000097514/ResultActivity.kt
+++ app/src/main/java/com/example/lab_week_02_b_00000097514/ResultActivity.kt
@@ -5,10 +5,16 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+import android.app.Activity
+import android.content.Intent
+import android.widget.Button
+
+
 
 class ResultActivity : AppCompatActivity() {
     companion object {
         private const val COLOR_KEY = "COLOR_KEY"
+        private const val ERROR_KEY = "ERROR_KEY"
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +23,21 @@ class ResultActivity : AppCompatActivity() {
             val colorCode = intent.getStringExtra(COLOR_KEY)
             val backgroundScreen =
                 findViewById<ConstraintLayout>(R.id.background_screen)
-            backgroundScreen.setBackgroundColor(Color.parseColor("#$colorCode"))
+            val backButton = findViewById<Button>(R.id.back_button)
+            backButton.setOnClickListener {
+                finish() // balik ke MainActivity
+            }
+            try {
+                backgroundScreen.setBackgroundColor(Color.parseColor("#$colorCode"))
+            }
+            catch (ex: IllegalArgumentException){
+                Intent().let{
+                        errorIntent ->
+                    errorIntent.putExtra(ERROR_KEY, true)
+                    setResult(Activity.RESULT_OK, errorIntent)
+                    finish()
+                }
+            }
             val resultMessage =
                 findViewById<TextView>(R.id.color_code_result_message)
             resultMessage.text = getString(R.string.color_code_result_message,
@@ -25,4 +45,5 @@ class ResultActivity : AppCompatActivity() {
         }
     }
 }
+
 
